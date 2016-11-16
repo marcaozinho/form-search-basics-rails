24456e83dc7499809b1f30c5fa1f781a84d60425
marcaozinho <aci.marcus@gmail.com>
2016-11-16 19:24:41 -0300

    customers search

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,9 +4,9 @@ class Customer < ActiveRecord::Base
 
   accepts_nested_attributes_for :address	
 
-  scope :search, ->(query) { where("name like ?", "%#{query}%") }
+  #scope :search, ->(query) { where("name like ?", "%#{query}%") }
 
-  #def self.search(query)
-  #	where("name like ?", "%#{query}%")
-  #end	
+  def self.search(query)
+  	where("name like ?", "%#{query}%")
+  end	
 end
